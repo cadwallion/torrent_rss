de787a03028a6d6665273e7177b7b2cbe2280e11
Andrew Nordman <cadwallion@gmail.com>
2013-05-06 23:50:23 -0500

    Adds verbosity to bin script

--- lib/torrent_rss/cli.rb
+++ lib/torrent_rss/cli.rb
@@ -8,6 +8,7 @@ module TorrentRSS
     method_option :feeds, aliases: '-f', type: :array, desc: 'Array of Feed URLs to parse and download from'
     method_option :directory, aliases: '-d', desc: 'Directory to download torrent files from'
     method_option :config, aliases: '-c', desc: 'Specify the configuration file.', default: '~/.torrent_rss'
+    method_option :verbose, type: :boolean, aliases: '-v', desc: 'Output verbosely'
     def fetch
       Config.config_file = options[:config]
       feed_urls = options[:feeds] || Config.feeds || []
@@ -15,7 +16,8 @@ module TorrentRSS
 
       feed_urls.each do |url|
         feed = Feed.new url
-        feed.fetch! directory: directory
+        verbose "Fetching for url #{url}"
+        feed.fetch! directory: directory, verbose: options[:verbose]
       end
     end
 
@@ -41,6 +43,10 @@ module TorrentRSS
         invoke :fetch
         sleep 60
       end
+    end
+
+    def verbose message
+      puts message if options[:verbose] 
     end
   end
 end
--- lib/torrent_rss/feed.rb
+++ lib/torrent_rss/feed.rb
@@ -8,7 +8,9 @@ module TorrentRSS
     def fetch! options = {}
       @directory = options[:directory]
       parsed_feed.entries.each do |entry|
+        puts "Parsing #{entry.entry_id}" if options[:verbose]
         if new_entry? entry
+          puts "Entry #{entry.entry_id} is new! Downloading..." if options[:verbose]
           torrent = Torrent.from_entry entry
           torrent.download @directory
           EntryLog.add entry.entry_id
@@ -22,6 +24,10 @@ module TorrentRSS
 
     def parsed_feed
       @feed ||= Feedzirra::Feed.fetch_and_parse @url
+    end
+
+    def verbose message
+      puts message if options[:verbose] 
     end
   end
 end
